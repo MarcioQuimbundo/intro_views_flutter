3c5ada2d7281e38ea2550c306653b7bc10938052
marcioquimbundo <marcioquimbundo@hotmail.com>
2019-05-21 17:43:08 +0100

    first commit

---
+++ CHANGELOG.md
@@ -0,0 +1,70 @@
+## 2.6.0
+* Fix problem padding top in title of pageViews.
+* Background property added that can be used to add full screen image to IntroViews.
+
+## 2.5.0
+* Added Next Button to move to the next screen. Overriding doneButtonPersist.
+* Added Back Button. Overrides showSkipButton starting from the second page.
+
+## 2.4.0
+**Feature Enhancement**
+* Exposed the way to change the distance a user needs to drag for a full transition to occur using `fullTransition` and its default value is set to `300.0`.
+* Added ability to insert a custom widget in the page bubble.
+* Changed `title`, `body`, `mainImage` PageViewModel types to be more dynamic.
+* Removed all `new` keywords.
+
+## 2.3.0
+**Feature Enhancement**
+* Exposed columnMainAxisAlignment to change mainAxisAlignment.
+* Updated Readme.
+
+## 2.2.4
+* Adjust the Scaffold in fullscreen.
+
+## 2.2.3
+* General Update.
+
+## 2.2.2
+* Added Widget testing.
+* Updated Readme.
+
+
+## 2.2.1
+* Added feature to override Text widget styles for skip and done button.
+* Added doneButtonPersist to show done button throughout the session.
+* Updated Readme.
+
+## 2.1.1
+* Fixed overflow for small screen apps.
+
+## 2.0.1
+**Major Updates**
+* Made intro views responsive.
+* Refracting page view model.
+  - Body title, expects Text Widget.
+  - Main image, expects Image Widget.
+  - Added TextStyle property to page view model that can be used to set styles for both title and body. 
+* Added TextStyle property for page indicator buttons which overrides previous button properties.
+* Updated Readme and example.
+
+## 1.0.3
+
+* Made intro views responsive.
+* Added onTapSkipButton voidCallback.
+* Updated Readme.
+
+## 1.0.2
+
+* Updated Readme.
+* Fixed bugs.
+* Fixed formatting issues.
+
+## 1.0.1
+
+* Updated Readme.
+* Fixed static analyzers issues.
+* Fixed formatting issues.
+
+## 1.0.0
+
+* Initial Release.
---
+++ example/lib/main.dart
@@ -0,0 +1,105 @@
+import 'package:flutter/material.dart';
+import 'package:intro_views_flutter/Models/page_view_model.dart';
+import 'package:intro_views_flutter/intro_views_flutter.dart';
+
+/// This is the main method of app, from here execution starts.
+void main() => runApp(App());
+
+/// App widget class
+
+class App extends StatelessWidget {
+  //making list of pages needed to pass in IntroViewsFlutter constructor.
+  final pages = [
+    PageViewModel(
+        pageColor: const Color(0xFF03A9F4),
+        // iconImageAssetPath: 'assets/air-hostess.png',
+        bubble: Image.asset('assets/air-hostess.png'),
+        body: Text(
+          'Haselfree  booking  of  flight  tickets  with  full  refund  on  cancelation',
+        ),
+        title: Text(
+          'Flights',
+        ),
+        textStyle: TextStyle(fontFamily: 'MyFont', color: Colors.white),
+        mainImage: Image.asset(
+          'assets/airplane.png',
+          height: 285.0,
+          width: 285.0,
+          alignment: Alignment.center,
+        )),
+    PageViewModel(
+      pageColor: const Color(0xFF8BC34A),
+      iconImageAssetPath: 'assets/waiter.png',
+      body: Text(
+        'We  work  for  the  comfort ,  enjoy  your  stay  at  our  beautiful  hotels',
+      ),
+      title: Text('Hotels'),
+      mainImage: Image.asset(
+        'assets/hotel.png',
+        height: 285.0,
+        width: 285.0,
+        alignment: Alignment.center,
+      ),
+      textStyle: TextStyle(fontFamily: 'MyFont', color: Colors.white),
+    ),
+    PageViewModel(
+      pageColor: const Color(0xFF607D8B),
+      iconImageAssetPath: 'assets/taxi-driver.png',
+      body: Text(
+        'Easy  cab  booking  at  your  doorstep  with  cashless  payment  system',
+      ),
+      title: Text('Cabs'),
+      mainImage: Image.asset(
+        'assets/taxi.png',
+        height: 285.0,
+        width: 285.0,
+        alignment: Alignment.center,
+      ),
+      textStyle: TextStyle(fontFamily: 'MyFont', color: Colors.white),
+    ),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'IntroViews Flutter', //title of app
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ), //ThemeData
+      home: Builder(
+        builder: (context) => IntroViewsFlutter(
+              pages,
+              onTapDoneButton: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => HomePage(),
+                  ), //MaterialPageRoute
+                );
+              },
+              pageButtonTextStyles: TextStyle(
+                color: Colors.white,
+                fontSize: 18.0,
+              ),
+            ), //IntroViewsFlutter
+      ), //Builder
+    ); //Material App
+  }
+}
+
+/// Home Page of our example app.
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home'),
+      ), //Appbar
+      body: Center(
+        child: Text("This is the home page of the app"),
+      ), //Center
+    ); //Scaffold
+  }
+}
